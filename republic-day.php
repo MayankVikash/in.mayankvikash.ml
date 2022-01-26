fe370caf9cbb5ea7796d2403458daba59fa413af
Mayank Vikash <74097099+MayankVikash@users.noreply.github.com>
2022-01-26 12:55:51 +0530

    Create republic-day.php

---
+++ republic-day.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Republic Day - Flag Animation | Mayank Vikash</title>
+    <meta name="description" content="Indian Flag Animation Made with CSS and Javascript.">
+    <meta name='og:title' content='Republic Day - Flag Animation | Mayank Vikash'>
+    <meta name='og:type' content='movie'>
+    <meta name='og:url' content='https://in.mayankvikash.ml/republic-day.php'>
+    <meta name='og:image' content='https://www.freeiconspng.com/uploads/ashoka-chakra-png-transparent-image-0.png'>
+    <meta name='og:site_name' content='Mayank Vikash'>
+    <meta name='og:description' content='Tiranga Animation made with CSS and Javascript.'>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+        
+        body {
+            background-color: rgb(51, 51, 51);
+            color: #fff;
+            overflow: hidden;
+        }
+        
+        .clr-orange,
+        .clr-white,
+        .clr-green {
+            height: 33.33vh;
+            width: 0;
+            position: absolute;
+            animation: grow 2s forwards;
+        }
+        
+        .clr-orange {
+            background: linear-gradient(to bottom, #ff8011, #ffae4a);
+            top: 0;
+            left: 0;
+        }
+        
+        .clr-white {
+            background: #fff;
+            top: 33.33vh;
+            right: 0;
+        }
+        
+        .clr-green {
+            background: linear-gradient(to bottom, #75fc3b, #46ad1b);
+            bottom: 0;
+            left: 0;
+        }
+        
+        @keyframes grow {
+            100% {
+                width: 100%;
+                transform: rotate(0deg);
+            }
+            0% {
+                transform: rotate(180deg);
+            }
+        }
+        
+        .ashoka-chakra {
+            position: absolute;
+            transform: translate(-50%, -50%);
+            left: 50%;
+            top: 50%;
+            visibility: hidden;
+            animation: spin 3s 2s linear infinite;
+        }
+        
+        @keyframes spin {
+            0% {
+                visibility: visible;
+            }
+            100% {
+                transform: translate(-50%, -50%) rotate(360deg);
+            }
+        }
+        
+        .details {
+            bottom: 0;
+            position: absolute;
+        }
+        
+        p {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 10px;
+            text-shadow: 1px 1px 2px #000;
+        }
+        
+        #button {
+            background: #000000;
+            background: linear-gradient(to bottom, #62f523, #575757);
+            border-radius: 30px;
+            text-shadow: 2px 2px 3px #000000;
+            font-family: Arial;
+            color: #ffffff;
+            font-size: 12px;
+            padding: 10px 15px 10px 15px;
+            text-decoration: none;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="clr-orange"></div>
+    <div class="clr-white"></div>
+    <div class="clr-green"></div>
+    <img src="https://www.freeiconspng.com/uploads/ashoka-chakra-png-transparent-image-0.png" alt="Ashoka Chakra
+    " class="ashoka-chakra" height="150">
+
+
+    <div class="details">
+        <button id="button" onClick="reload()">Reload</button> <br>
+        <p>Made by <b>Mayank Vikash</b> <br> © 2022 in.mayankvikash.ml</p>
+    </div>
+    <script>
+        function reload() {
+            window.location.reload();
+        }
+    </script>
+</body>
+
+</html>
